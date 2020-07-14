dcc214f5f08887954f6cfcb35b3e38c6ec3e6c41
Barry Yu <doung1118@gmail.com>
2020-07-14 18:21:55 +0800

    finshed mission exam

---
+++ main.js
@@ -0,0 +1,33 @@
+function fib(num) {
+
+  const arr = [0, 1]
+  const mapper = {}
+  let counter = 0
+
+  for (let i = 2; i <= num; i++) {
+
+
+    let a = arr[i - 1];
+    let b = arr[i - 2];
+    arr.push(a + b);
+
+  }
+  return arr[num];
+
+  for (let elem in arr) {
+    mapper[counter] = arr[elem]
+    counter++
+  }
+
+
+
+
+
+  return mapper
+};
+
+
+
+
+
+console.log(fibIt(10)); 
